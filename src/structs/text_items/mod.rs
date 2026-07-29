cf45f7f36772239648a5b138015091fbaaefef98
Daniel Weber <gravytoast@pm.me>
2026-07-29 15:36:13 +1000

    feat: add functions to write/read TextItems to/from JSON

--- src/structs/text_items/mod.rs
+++ src/structs/text_items/mod.rs
@@ -1,5 +1,7 @@
 pub mod buffer;
+pub mod json;
 pub mod tokenise;
 
 pub use buffer::get_text_item_buffer;
+pub use json::{json_to_text_items, text_items_to_json};
 pub use tokenise::tokenise_items;
