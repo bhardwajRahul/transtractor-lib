9415f1ef04042ea01094128937b4fe9cb6048017
Daniel <weberdk01@gmail.com>
2026-08-27 11:02:18 +0000

    fix: implement trigger counts to fix parsing of amex statements

--- src/configs/registry/us/axp_platinum_1.rs
+++ src/configs/registry/us/axp_platinum_1.rs
@@ -12,29 +12,33 @@ pub fn get_config() -> StatementConfig {
         account_examples: vec!["Platinum Card".to_string()],
 
         account_number_terms: vec!["Account Ending".to_string()],
+        account_number_trigger_count: 1,
         account_number_patterns: vec![Regex::new(r"\b\d-\d+\b").unwrap()],
         account_number_alignment: "x1".to_string(),
         account_number_alignment_tol: 5,
 
         opening_balance_terms: vec!["Previous Balance".to_string()],
+        opening_balance_trigger_count: 3,
         opening_balance_formats: vec!["format2".to_string()],
         opening_balance_alignment: "y1".to_string(),
         opening_balance_alignment_tol: 5,
         opening_balance_invert: true,
 
         closing_balance_terms: vec!["New Balance".to_string()],
+        closing_balance_trigger_count: 4,
         closing_balance_formats: vec!["format2".to_string()],
         closing_balance_alignment: "y1".to_string(),
         closing_balance_alignment_tol: 5,
         closing_balance_invert: true,
 
         start_date_terms: vec![],
+        start_date_trigger_count: 1,
         start_date_formats: vec![],
         start_date_alignment: "y1".to_string(),
         start_date_alignment_tol: 5,
-        start_date_first_match: false,
 
         transaction_terms: vec!["Total Payments and Credits".to_string()],
+        transaction_trigger_count: 1,
         transaction_terms_stop: vec!["Total Fees for this Period".to_string()],
         transaction_formats: vec![vec![
             "date".to_string(),
